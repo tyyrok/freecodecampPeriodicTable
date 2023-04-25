4ed97ffe68416c890936b4fd40e338fca392da97
tyyrok <tyyrok@gmail.com>
2023-04-25 10:15:39 +0000

    fix: fix an incorrect output

--- element.sh
+++ element.sh
@@ -5,13 +5,21 @@ then
   echo -e "Please provide an element as an argument."
 else
   #Check if element exists in table?
-  CHECK_ELEMENT_IN_DB=$($PSQL "SELECT name FROM elements WHERE atomic_number='$1' OR symbol='$1'")
+  CHECK_ELEMENT_IN_DB=$($PSQL "SELECT name, atomic_mass, melting_point_celsius, boiling_point_celsius, type, symbol FROM elements INNER JOIN properties USING (atomic_number) INNER JOIN types USING (type_id) WHERE atomic_number='$1' OR symbol='$1'")
+
   #If no - print error
   if [[ -z $CHECK_ELEMENT_IN_DB ]]
   then
     echo -e "I could not find that element in the database."
   #If yes - print a details about element
   else
-    echo -e "\nBingo!"
+    #echo $CHECK_ELEMENT_IN_DB
+    ELEMENT_NAME=$(echo $CHECK_ELEMENT_IN_DB | cut -d "|" -f 1)
+    ELEMENT_MASS=$(echo $CHECK_ELEMENT_IN_DB | cut -d "|" -f 2)
+    ELEMENT_MELTING_POINT=$(echo $CHECK_ELEMENT_IN_DB | cut -d "|" -f 3)
+    ELEMENT_BOILING_POINT=$(echo $CHECK_ELEMENT_IN_DB | cut -d "|" -f 4)
+    ELEMENT_TYPE=$(echo $CHECK_ELEMENT_IN_DB | cut -d "|" -f 5)
+    ELEMENT_SYMBOL=$(echo $CHECK_ELEMENT_IN_DB | cut -d "|" -f 6)
+    echo -e "The element with atomic number $1 is $ELEMENT_NAME ($ELEMENT_SYMBOL). It's a $ELEMENT_TYPE, with a mass of $ELEMENT_MASS amu. $ELEMENT_NAME has a melting point of $ELEMENT_MELTING_POINT celsius and a boiling point of $ELEMENT_BOILING_POINT celcius."
   fi
 fi
